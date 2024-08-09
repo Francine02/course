e2a7b1632df432bd2b463ea8fa8496ccf473f66d
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-09 16:51:57 -0300

    Configurado o aplication.properties, adicionado dependências e JPA

--- course/src/main/java/com/example/course/entities/User.java
+++ course/src/main/java/com/example/course/entities/User.java
@@ -1,6 +1,16 @@
 package com.example.course.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
 public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
--- course/src/main/java/controllers/UserResource.java
+++ course/src/main/java/controllers/UserResource.java
@@ -1,4 +1,4 @@
-package resources;
+package controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
