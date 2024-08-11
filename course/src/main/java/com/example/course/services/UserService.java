6a6f9ddb78c185749abbe182abd5b5a7608781eb
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-11 17:26:58 -0300

    Exceções para o findById

--- course/src/main/java/com/example/course/services/UserService.java
+++ course/src/main/java/com/example/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
+import com.example.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> user = userRepository.findById(id);
-        return user.get();//Para retornar o que tem dentro
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));//Para retornar o que tem dentro
     }
 
     public User insert(User user){
---
+++ course/src/main/java/com/example/course/services/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.course.services.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice //Para interceptar as exceções
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)//Intercepta exceções desse tipo
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(erro);
+    }
+}
